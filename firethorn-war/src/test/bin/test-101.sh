6ab407365846fd6fe090f568224e85b19508798d
Dave Morris <zrq-github@metagrid.co.uk>
2012-12-19 19:23:54 +0000

    Bug fixes

--- firethorn-war/src/test/bin/test-101.sh
+++ firethorn-war/src/test/bin/test-101.sh
@@ -15,12 +15,18 @@ unique()
 
 #
 # List the JDBC resources.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/jdbc/resource/select"
 
 #
 # Create our JDBC resource.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     --data "jdbc.resource.create.url=spring:RoeTWOMASS" \
@@ -29,24 +35,36 @@ curl \
 
 #
 # Get the resource details.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/jdbc/resource/1"
 
 #
 # List the resource schema.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/jdbc/resource/1/schemas/select"
 
 #
 # List the schema tables.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/jdbc/schema/1/tables/select"
 
 #
 # Get a table details.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/jdbc/table/5"
@@ -55,12 +73,18 @@ curl \
 
 #
 # List the ADQL resources.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/adql/resource/select"
 
 #
-# Create our ADQL resource.
+# Create an ADQL resource.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     --data "adql.resource.create.name=adql-resource-$(unique)" \
@@ -68,18 +92,27 @@ curl \
 
 #
 # Get the resource details.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/adql/resource/2"
 
 #
 # List the resource schema.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/adql/resource/2/schemas/select"
 
 #
 # Create our ADQL schema.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     --data "adql.resource.schema.create.name=created_schema" \
@@ -87,6 +120,9 @@ curl \
 
 #
 # List the resource schema.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/adql/resource/2/schemas/select"
@@ -95,12 +131,18 @@ curl \
 
 #
 # List the schema tables.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/adql/schema/2/tables/select"
 
 #
 # Import a JDBC table into our ADQL schema.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     --data "adql.schema.table.import.base=${basename}/jdbc/table/5" \
@@ -108,6 +150,9 @@ curl \
 
 #
 # Import a JDBC table with a new name.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     --data "adql.schema.table.import.name=imported_table" \
@@ -116,6 +161,9 @@ curl \
 
 #
 # List the schema tables.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/adql/schema/2/tables/select"
@@ -124,12 +172,18 @@ curl \
 
 #
 # Get the details for a table.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/adql/table/12"
 
 #
 # List the table columns.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/adql/table/12/columns/select"
@@ -138,12 +192,18 @@ curl \
 
 #
 # List the ADQL schema.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/adql/resource/2/schemas/select"
 
 #
 # Import the tables from a JDBC schema.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     --data "adql.resource.schema.import.name=imported_schema" \
@@ -152,6 +212,9 @@ curl \
 
 #
 # List the ADQL schema.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/adql/resource/2/schemas/select"
@@ -160,12 +223,18 @@ curl \
 
 #
 # List the tables in a schema.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/adql/schema/3/tables/select"
 
 #
 # List the columns in a table.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     "${basename}/adql/table/24/columns/select"
@@ -174,6 +243,9 @@ curl \
 
 #
 # Create an ADQL query.
+echo ""
+echo "----"
+echo ""
 curl \
     -H 'Accept: application/json' \
     --data-urlencode "adql.resource.query.create.name=test-query" \
