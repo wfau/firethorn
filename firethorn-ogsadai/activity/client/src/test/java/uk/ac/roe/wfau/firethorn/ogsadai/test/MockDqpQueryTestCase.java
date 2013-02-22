7e982e171e10d48a62d1bcb3f15e29b1ed38df83
Dave Morris <zrq-github@metagrid.co.uk>
2013-02-22 14:07:17 +0000

    Created mock and test DQP config

--- firethorn-ogsadai/activity/client/src/test/java/uk/ac/roe/wfau/firethorn/ogsadai/test/MockDqpQueryTestCase.java
+++ firethorn-ogsadai/activity/client/src/test/java/uk/ac/roe/wfau/firethorn/ogsadai/test/MockDqpQueryTestCase.java
@@ -1,4 +1,7 @@
 /*
+
+ *  
+ *  
  *  Copyright (C) 2012 Royal Observatory, University of Edinburgh, UK
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -57,7 +60,7 @@ import uk.org.ogsadai.client.toolkit.activities.transform.TupleToByteArrays;
  *
  *
  */
-public class DqpQueryTestCase
+public class MockDqpQueryTestCase
 extends SimpleQueryTestBase
     {
 
@@ -65,7 +68,7 @@ extends SimpleQueryTestBase
      * Our debug logger.
      * 
      */
-    private static Log log = LogFactory.getLog(DqpQueryTestCase.class);
+    private static Log log = LogFactory.getLog(MockDqpQueryTestCase.class);
 
     /**
      * Our test service endpoint.
@@ -193,57 +196,6 @@ extends SimpleQueryTestBase
             "    neighbour.slaveObjID = twomass.pts_key" +
             " AND" +
             "    neighbour.distanceMins < 1E-3"
-            );
-        }
-
-
-    /**
-     * TWOMASS and UKIDSSDR1, using DQP, using GT and LT on both tables.
-     *
-     */
-    @Test
-    public void test100()
-    throws Exception
-        {
-        execute(
-            endpoint,
-            "mydqp",
-            " SELECT" +
-            "     twomass.ra AS tmra," +
-            "     ukidss.ra AS ukra," +
-            "     twomass.ra-ukidss.ra AS difra," +
-            "     twomass.dec AS tmdec," +
-            "     ukidss.dec AS ukdec," +
-            "     twomass.ra-ukidss.ra AS difdec," +
-            "     neighbour.distanceMins AS dist" +
-            " FROM" +
-            "     JDBC_5 AS twomass" +
-            " CROSS JOIN" +
-            "     JDBC_120 AS ukidss" +
-            " CROSS JOIN" +
-            "     JDBC_41 AS neighbour" +
-            " WHERE" +
-            "     twomass.ra >= '55.0'" +
-            " AND" +
-            "     twomass.ra <= '55.9'" +
-            " AND" +
-            "     twomass.dec >= '20.0'" +
-            " AND" +
-            "     twomass.dec <= '22.9'" +
-            " AND" +
-            "     ukidss.ra >= '55.0'" +
-            " AND" +
-            "     ukidss.ra <= '55.9'" +
-            " AND" +
-            "     ukidss.dec >= '20.0'" +
-            " AND" +
-            "     ukidss.dec <= '22.9'" +
-            " AND" +
-            "     neighbour.masterObjID = ukidss.sourceID" +
-            " AND" +
-            "     neighbour.slaveObjID = twomass.pts_key" +
-            " AND" +
-            "     neighbour.distanceMins < 1E-3"
             );
         }
     }
---
+++ firethorn-ogsadai/activity/client/src/test/java/uk/ac/roe/wfau/firethorn/ogsadai/test/TestDqpQueryTestCase.java
@@ -0,0 +1,129 @@
+/*
+
+ *  
+ *  
+ *  Copyright (C) 2012 Royal Observatory, University of Edinburgh, UK
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package uk.ac.roe.wfau.firethorn.ogsadai.test;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.junit.Test;
+
+import java.net.URL;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+import uk.org.ogsadai.resource.ResourceID;
+import uk.org.ogsadai.resource.request.RequestStatus;
+
+import uk.org.ogsadai.client.toolkit.Server;
+import uk.org.ogsadai.client.toolkit.ServerProxy;
+import uk.org.ogsadai.client.toolkit.PipelineWorkflow;
+
+import uk.org.ogsadai.client.toolkit.presentation.jersey.JerseyServer;
+
+import uk.org.ogsadai.client.toolkit.RequestResource;
+import uk.org.ogsadai.client.toolkit.RequestExecutionType;
+
+import uk.org.ogsadai.client.toolkit.DataSourceResource;
+import uk.org.ogsadai.client.toolkit.DataRequestExecutionResource;
+
+import uk.org.ogsadai.client.toolkit.resource.ResourceFactory;
+
+import uk.org.ogsadai.client.toolkit.activities.sql.SQLQuery;
+import uk.org.ogsadai.client.toolkit.activities.sql.SQLBulkLoadTuple;
+
+import uk.org.ogsadai.client.toolkit.activities.delivery.WriteToDataSource;
+import uk.org.ogsadai.client.toolkit.activities.delivery.DeliverToRequestStatus;
+
+import uk.org.ogsadai.client.toolkit.activities.transform.TupleToCSV;
+import uk.org.ogsadai.client.toolkit.activities.transform.TupleToByteArrays;
+
+/**
+ *
+ *
+ */
+public class TestDqpQueryTestCase
+extends SimpleQueryTestBase
+    {
+
+    /**
+     * Our debug logger.
+     * 
+     */
+    private static Log log = LogFactory.getLog(TestDqpQueryTestCase.class);
+
+    /**
+     * Our test service endpoint.
+     * 
+     */
+    public static final String endpoint = "http://localhost:8081/albert/services/" ;
+
+    /**
+     * TWOMASS and UKIDSSDR1, using test DQP, using GT and LT on both tables.
+     *
+     */
+    @Test
+    public void test001()
+    throws Exception
+        {
+        execute(
+            endpoint,
+            "testdqp",
+            " SELECT" +
+            "     twomass.ra AS tmra," +
+            "     ukidss.ra AS ukra," +
+            "     twomass.ra-ukidss.ra AS difra," +
+            "     twomass.dec AS tmdec," +
+            "     ukidss.dec AS ukdec," +
+            "     twomass.ra-ukidss.ra AS difdec," +
+            "     neighbour.distanceMins AS dist" +
+            " FROM" +
+            "     JDBC_5 AS twomass" +
+            " CROSS JOIN" +
+            "     JDBC_120 AS ukidss" +
+            " CROSS JOIN" +
+            "     JDBC_41 AS neighbour" +
+            " WHERE" +
+            "     twomass.ra >= '55.0'" +
+            " AND" +
+            "     twomass.ra <= '55.9'" +
+            " AND" +
+            "     twomass.dec >= '20.0'" +
+            " AND" +
+            "     twomass.dec <= '22.9'" +
+            " AND" +
+            "     ukidss.ra >= '55.0'" +
+            " AND" +
+            "     ukidss.ra <= '55.9'" +
+            " AND" +
+            "     ukidss.dec >= '20.0'" +
+            " AND" +
+            "     ukidss.dec <= '22.9'" +
+            " AND" +
+            "     neighbour.masterObjID = ukidss.sourceID" +
+            " AND" +
+            "     neighbour.slaveObjID = twomass.pts_key" +
+            " AND" +
+            "     neighbour.distanceMins < 1E-3"
+            );
+        }
+    }
+
