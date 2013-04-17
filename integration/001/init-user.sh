07335b96a1bc026179c171109ad59c47dcd3fad8
Dave Morris <zrq-github@metagrid.co.uk>
2013-04-17 18:22:26 +0100

    Hsqldb server and Liquibase config

--- integration/001/init-user.sh
+++ integration/001/init-user.sh
@@ -37,7 +37,7 @@ jdbcresource=$(cat jdbc-user-resource.json | ident)
 #
 # ** Needs at least one table in the schema.
 POST "${jdbcresource?}/schemas/select" \
-    -d "jdbc.resource.schema.select.name=public" \
+    -d "jdbc.resource.schema.select.name=PUBLIC.PUBLIC" \
     | tee user-schema.json | ./pp
 userschema=$(cat user-schema.json | ident)
 
