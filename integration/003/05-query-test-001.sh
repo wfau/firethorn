d2e48a2800f9399ffa983d5552ccf70297caf174
Dave Morris <zrq-github@metagrid.co.uk>
2013-05-30 13:46:37 +0100

    ....

--- integration/003/05-query-test-001.sh
+++ integration/003/05-query-test-001.sh
@@ -27,6 +27,35 @@ cat > "${queryfile?}" << 'EOF'
 EOF
 
 #
+# With the 'AS id' alias on the nested query, we get the following ADQL and SQL.
+
+#  "osql" : "SELECT CrossMatch.distanceMins AS DistanceMins\nFROM UKIDSSDR5PLUS.dbo.lasSourceXDR7PhotoObj AS CrossMatch , (SELECT s.sourceID AS id\nFROM UKIDSSDR5PLUS.dbo.lasSource AS s\nWHERE s.ra > 0 AND s.ra < 13.94 AND s.dec > -12.0 AND s.dec < -8.5 AND s.mergedClass = 1\nGROUP BY s.sourceID) AS T \nWHERE CrossMatch.distanceMins < 2/60.0 AND CrossMatch.sdssType = 3 AND CrossMatch.sdssPrimary = 1 AND T.sourceID = CrossMatch.masterObjID",
+#  "adql" : "SELECT DistanceMins\nFROM ukidss.lasSourceXDR7PhotoObj AS CrossMatch CROSS JOIN (SELECT s.SourceId AS id\nFROM ukidss.lasSource AS s\nWHERE ra > 0 AND ra < 13.94 AND dec > -12.0 AND dec < -8.5 AND mergedclass = 1\nGROUP BY s.SourceId) AS T\nWHERE DistanceMins < 2/60.0 AND sdsstype = 3 AND sdssPrimary = 1 AND T.id = CrossMatch.masterObjID",
+
+#
+# The generated SQL contains 'AS id' in the nested query, but joins on the original column name 'T.sourceID' not 'T.id'.
+"
+    SELECT
+        ...
+    FROM
+        UKIDSSDR5PLUS.dbo.lasSourceXDR7PhotoObj AS CrossMatch ,
+        (
+        SELECT
+            s.sourceID AS id
+        FROM
+            ...
+        WHERE
+            ....
+        GROUP BY
+            s.sourceID
+        ) AS T
+    WHERE
+        ....
+    AND
+        T.sourceID = CrossMatch.masterObjID
+"
+
+#
 # This works (without the alias on s.SourceId)
 cat > "${queryfile?}" << 'EOF'
 
