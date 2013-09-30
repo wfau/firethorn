1c7348d81b8cffd61ea3615f3a6ac6554d6d1c61
stvoutsin <steliosvoutsinas@yahoo.com>
2013-09-30 16:06:02 +0300

    Arithmetic change - Fixed null type error

--- firethorn-core/src/main/java/uk/ac/roe/wfau/firethorn/adql/parser/AdqlParserImpl.java
+++ firethorn-core/src/main/java/uk/ac/roe/wfau/firethorn/adql/parser/AdqlParserImpl.java
@@ -906,7 +906,13 @@ implements AdqlParser
                 t2 = a2.column().meta().adql().type();
                 }
             }
-    
+        if (t1==null){
+        	t1 = AdqlColumn.Type.DOUBLE;
+        }
+        if (t2==null){
+        	t2 = AdqlColumn.Type.DOUBLE;
+        }
+        
         if (oper.getName()=="%") {
         	return_type = AdqlColumn.Type.INTEGER;
         } else if (t1==t2){
@@ -974,7 +980,8 @@ implements AdqlParser
         			return_type = AdqlColumn.Type.FLOAT;
 
         }
-       		
+        log.debug("  return_type******************** [{}]",return_type);
+
         return new SelectFieldImpl(
             "operation",
             return_type
