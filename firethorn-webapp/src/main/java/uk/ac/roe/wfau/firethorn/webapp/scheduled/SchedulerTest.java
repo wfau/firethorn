858d7a48614eb372d2b35368cc8fb358a88087ca
Dave Morris <zrq-github@metagrid.co.uk>
2013-11-28 14:02:12 +0100

    Added scheduler test

---
+++ firethorn-webapp/src/main/java/uk/ac/roe/wfau/firethorn/webapp/scheduled/SchedulerTest.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package uk.ac.roe.wfau.firethorn.webapp.scheduled;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ *
+ */
+@Slf4j
+public class SchedulerTest
+	{
+
+	@Scheduled(fixedDelay=5000)
+	public void something() {
+	    log.debug("Scheduled task");
+
+	    try {
+		    log.debug("Sleeping");
+	    	Thread.sleep(1000);
+		    log.debug("Awake");
+	    	}
+	    catch (Exception ouch)
+	    	{
+	    	log.debug("Exception [{}]", ouch.getMessage());
+	    	}
+		}	
+	}
