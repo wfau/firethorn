4c0be64e34c13f711a6ab07026e5d5a191c550c4
Dave Morris <zrq-github@metagrid.co.uk>
2013-11-28 14:32:06 +0100

    ....

--- firethorn-webapp/src/main/java/uk/ac/roe/wfau/firethorn/webapp/scheduled/SchedulerTest.java
+++ firethorn-webapp/src/main/java/uk/ac/roe/wfau/firethorn/webapp/scheduled/SchedulerTest.java
@@ -5,11 +5,13 @@ package uk.ac.roe.wfau.firethorn.webapp.scheduled;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 /**
  *
  */
 @Slf4j
+@Service
 public class SchedulerTest
 	{
 
