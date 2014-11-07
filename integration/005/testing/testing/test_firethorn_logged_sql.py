9a5c850aff555006e79a366a56719e62dbf495c2
stvoutsin <steliosvoutsinas@yahoo.com>
2014-11-07 15:15:03 +0200

    logging fixes

--- integration/005/testing/testing/test_firethorn_logged_sql.py
+++ integration/005/testing/testing/test_firethorn_logged_sql.py
@@ -168,8 +168,8 @@ class test_firethorn(unittest.TestCase):
                 if (config.test_is_continuation):
                     total_unique_queries_qry = "select count(*) from queries where  queryrunID='" + queryrunID + "'"
                     self.total_unique_queries = reporting_sqlEng.execute_sql_query(total_unique_queries_qry, config.reporting_database)[0][0]
-                    total_queries_qry = "select sum(query_count) from queries where  queryrunID='" + queryrunID + "'"
-                    self.total_queries = reporting_sqlEng.execute_sql_query(total_queries_qry, config.reporting_database)[0][0]
+                    #total_queries_qry = "select sum(query_count) from queries where  queryrunID='" + queryrunID + "'"
+                    #self.total_queries = reporting_sqlEng.execute_sql_query(total_queries_qry, config.reporting_database)[0][0]
                     total_failed_qry = "select count(*) from queries where  queryrunID='" + queryrunID + "' and test_passed<=0"
                     self.total_failed = reporting_sqlEng.execute_sql_query(total_failed_qry, config.reporting_database)[0][0]
             except Exception as e:
@@ -187,7 +187,7 @@ class test_firethorn(unittest.TestCase):
                 firethorn_error_message = ""
                 sql_error_message =  ""
                 logging.info("Query : " +  query)
-		
+		self.total_queries = self.total_queries + 1
                 try:
                     if (config.test_is_continuation):
                         check_duplicate_query = "select count(*), queryid, query_count from queries where query_hash='" + querymd5 + "' and queryrunID='" + queryrunID + "'"		          
@@ -265,20 +265,20 @@ class test_firethorn(unittest.TestCase):
                     params = (query, queryrunID, querymd5, 1,  query_timestamp, sql_row_length, firethorn_row_length, firethorn_duration, sql_duration, test_passed, firethorn_version, str(firethorn_error_message).encode('utf-8'), str(sql_error_message).encode('utf-8'), java_version, firethorn_changeset, sys_platform, sys_timestamp )
                     report_query = "INSERT INTO queries (query, queryrunID, query_hash, query_count, query_timestamp, direct_sql_rows, firethorn_sql_rows, firethorn_duration, sql_duration, test_passed, firethorn_version, firethorn_error_message, sql_error_message, java_version, firethorn_changeset, sys_platform, sys_timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)" 
                     reporting_sqlEng.execute_insert(report_query, config.reporting_database, params=params)
-                    self.total_queries = self.total_queries + 1
+                    #self.total_queries = self.total_queries + 1
 
                 else :
                     if (queryid!=None and query_count!=None and (continue_from_here_flag==True)):
 			logging.info("Query has been run already..Skipping & updating count")
                         update_query = "UPDATE queries SET query_count=" + str(query_count + 1) + " WHERE queryid=" + str(queryid)
                         update_results = reporting_sqlEng.execute_update(update_query, config.reporting_database)
-                        self.total_queries = self.total_queries + 1
+                        #self.total_queries = self.total_queries + 1
 
 
                 logging.info("Total queries: "  + str(self.total_queries))
                 logging.info("Total unique queries: "  + str(self.total_unique_queries))
                 logging.info("Total failed: " + str(self.total_failed))
-                logging.info("Coverage percentage: "  + str(round((self.total_queries/total_available_queries)*100,2)) + "%")
+                logging.info("Coverage percentage: "  + str(round((float(self.total_queries)/float(total_available_queries))*100,2)) + "%")
                 logging.info("Success percentage: " +  str(round(100-(float(self.total_failed)/float(self.total_unique_queries))*100,2)) + "%")
                
 
