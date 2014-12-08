6c79a8c71f2f3322b2699a4e65c1ea4e726cdc07
stvoutsin <steliosvoutsinas@yahoo.com>
2014-12-08 23:51:31 +0200

    MUL -> MULT in ColumnTypeTestCase

--- firethorn-core/src/test/java/uk/ac/roe/wfau/firethorn/adql/query/atlas/ColumnTypeTestCase.java
+++ firethorn-core/src/test/java/uk/ac/roe/wfau/firethorn/adql/query/atlas/ColumnTypeTestCase.java
@@ -61,7 +61,7 @@ public class ColumnTypeTestCase
             "    {ATLAS_VERSION}.dbo.atlassource.uclass AS uclass," +
             "    {ATLAS_VERSION}.dbo.atlassource.uclass + {ATLAS_VERSION}.dbo.atlassource.cueventid  AS SUM," +
             "    {ATLAS_VERSION}.dbo.atlassource.uclass - {ATLAS_VERSION}.dbo.atlassource.framesetid AS SUB," +
-            "    {ATLAS_VERSION}.dbo.atlassource.uclass * {ATLAS_VERSION}.dbo.atlassource.umgpnt     AS MUL," +
+            "    {ATLAS_VERSION}.dbo.atlassource.uclass * {ATLAS_VERSION}.dbo.atlassource.umgpnt     AS MULT," +
             "    {ATLAS_VERSION}.dbo.atlassource.uclass / {ATLAS_VERSION}.dbo.atlassource.lambda     AS DIV" +
             " FROM" +
             "    {ATLAS_VERSION}.dbo.atlassource",
