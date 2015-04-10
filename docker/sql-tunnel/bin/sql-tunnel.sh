fbafe46d91b02a324fb354cfbd12ad883763cc14
Dave Morris <zrq-github@metagrid.co.uk>
2015-04-10 02:45:22 +0100

    Added sql-tunnel script

---
+++ docker/sql-tunnel/bin/sql-tunnel.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# <meta:header>
+#   <meta:licence>
+#     Copyright (c) 2015, ROE (http://www.roe.ac.uk/)
+#
+#     This information is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This information is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#  
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#   </meta:licence>
+# </meta:header>
+#
+#
+
+echo "Tunnel host [${tunnel:?}]"
+echo "Target host [${target:?}]"
+
+SSH_AUTH_SOCK='/ssh_auth_sock'
+
+#
+# Proxy a SQLServer connection.
+ssh -v -C -L "*:1433:${target:?}:1433" "${tunnel:?}"
+
+
