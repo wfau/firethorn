25be6f90506c09a8a6a9fe013ebfbdc9facf2115
stvoutsin <steliosvoutsinas@yahoo.com>
2015-04-24 20:07:39 +0300

    Pyrothorn docker updated

--- docker/pyrosql/5.7/install.sh
+++ docker/pyrosql/5.7/install.sh
@@ -23,20 +23,26 @@ fi
 
 mysql -hlocalhost -uroot -e "CREATE DATABASE pyrothorn_testing;"
 mysql -hlocalhost -uroot -Dpyrothorn_testing -e "CREATE TABLE IF NOT EXISTS queries (
-      queryid int(10) unsigned NOT NULL AUTO_INCREMENT,
-      queryrunID text NOT NULL,
-      query_timestamp varchar(120) NOT NULL,
-      query text NOT NULL,
-      direct_sql_rows int(11) NOT NULL,
-      firethorn_sql_rows int(11) NOT NULL,
-      firethorn_duration FLOAT NOT NULL,
-      sql_duration FLOAT NOT NULL,
-      test_passed tinyint(1) NOT NULL,
-      firethorn_version varchar(60) NOT NULL,
-      error_message varchar(60) NOT NULL,
-      PRIMARY KEY (queryid)
-    ) ;
-   "
+  queryid int(10) unsigned NOT NULL AUTO_INCREMENT,
+  queryrunID text NOT NULL,
+  query_hash text NOT NULL,
+  query_count int(10) unsigned NOT NULL,
+  query_timestamp varchar(120) NOT NULL,
+  query text NOT NULL,
+  direct_sql_rows int(11) NOT NULL,
+  firethorn_sql_rows int(11) NOT NULL,
+  firethorn_duration varchar(60) NOT NULL,
+  sql_duration varchar(60) NOT NULL,
+  test_passed tinyint(1) NOT NULL,
+  firethorn_version varchar(60) NOT NULL,
+  firethorn_error_message text NOT NULL,
+  sql_error_message text NOT NULL,
+  java_version varchar(120) NOT NULL,
+  sys_timestamp varchar(60) NOT NULL,
+  firethorn_changeset varchar(60) NOT NULL,
+  sys_platform varchar(60) NOT NULL,
+  PRIMARY KEY (queryid)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;"
 
 
 # shut down mysqld
