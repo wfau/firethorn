988de80f601f425deadc01088d49b7c990cdc35b
stvoutsin <steliosvoutsinas@yahoo.com>
2015-05-15 15:59:39 +0300

    Added JUnit test case #873 from redmine

---
+++ firethorn-core/src/test/java/uk/ac/roe/wfau/firethorn/adql/query/redmine/RedmineBug873TestCase.java
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (C) 2013 Royal Observatory, University of Edinburgh, UK
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package uk.ac.roe.wfau.firethorn.adql.query.redmine;
+
+import org.junit.Test;
+
+import uk.ac.roe.wfau.firethorn.adql.query.AdqlQuery.Syntax.Level;
+import uk.ac.roe.wfau.firethorn.adql.query.AdqlQuery.Syntax.State;
+import uk.ac.roe.wfau.firethorn.adql.query.atlas.AtlasQueryTestBase;
+import uk.ac.roe.wfau.firethorn.meta.adql.AdqlColumn;
+
+/**
+ * JUnit test case for RedMine issue.
+ * http://redmine.roe.ac.uk/issues/873
+ *
+ */
+public class RedmineBug873TestCase
+    extends AtlasQueryTestBase
+    {
+
+    /**
+     * 
+     */
+    @Test
+    public void test001()
+    throws Exception
+        {
+        validate(
+            Level.LEGACY,
+            State.VALID,
+            "SELECT Filter.* from Filter WHERE filterID>0"
+            
+            );
+        
+
+        }
+    }
