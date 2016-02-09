bb31cacec52b38b4dc1057245ac6109f336d1478
stvoutsin <steliosvoutsinas@yahoo.com>
2016-02-09 15:23:09 +0200

    apache conf changes

--- integration/005/scripts/setup/apache-tap-config-script.sh
+++ integration/005/scripts/setup/apache-tap-config-script.sh
@@ -3,11 +3,16 @@ cat >> /etc/apache2/apache2.conf <<EOF
 # mod_proxy setup.
 ProxyRequests Off
 ProxyPreserveHost On
-ProxyPassMatch ^/(firethorn\/tap\/.*)$   http://${gillianip:?}:8080/\$1 retry=0 connectiontimeout=14400 timeout=14400
-ProxyPassReverse  ^/(firethorn\/tap\/.*)$   http://${gillianip:?}:8080/\$1 
 
-ProxyPassMatch ^/(firethorn\/adql\/table\/.*\/votable)$   http://${gillianip:?}:8080/\$1 retry=0 connectiontimeout=14400 timeout=14400
-ProxyPassReverse  ^/(firethorn\/adql\/table\/.*\/votable)$ http://${gillianip:?}:8080/\$1 
+ProxyPassMatch ^/firethorn\/adql\/table\/(.*)\/votable$    http://${gillianip:?}:8080/firethorn/adql/table/$1/votable retry=0 connectiontimeout=1$
+ProxyPassReverse  ^/firethorn\/adql\/table\/(.*)\/votable$ http://${gillianip:?}:8080/firethorn/adql/table/$1/votable
+
+ProxyPassMatch ^/firethorn\/tap\/atlasdr1\/(.*)$  http://{gillianip:?}:8080/firethorn/tap/4096001/$1 retry=0 connectiontimeout=14400 timeout=144$
+ProxyPassReverse  ^/firethorn\/tap\/atlasdr1\/(.*)$  http://{gillianip:?}:8080/firethorn/tap/4096001/$1
+
+ProxyPassMatch ^/firethorn\/tap\/(.*)$  http://{gillianip:?}:8080/firethorn/tap/$1 retry=0 connectiontimeout=14400 timeout=144$
+ProxyPassReverse  ^/firethorn\/tap\/(.*)$  http://{gillianip:?}:8080/firethorn/tap/$1
+
 
 <Proxy *>
 Order deny,allow
