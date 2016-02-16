d893b3a512b975805e62dcec162b626838899825
stvoutsin <steliosvoutsinas@yahoo.com>
2016-02-16 19:28:02 +0200

    VARCHAR(MAX) for tap_schema

--- firethorn-webapp/src/main/webapp/WEB-INF/data/sqlserver_tap_schema.sql
+++ firethorn-webapp/src/main/webapp/WEB-INF/data/sqlserver_tap_schema.sql
@@ -1,9 +1,9 @@
 
-CREATE TABLE "TAP_SCHEMA"."schemas" ("schema_name" VARCHAR(1000), "description" VARCHAR(1000), "utype" VARCHAR(1000), PRIMARY KEY("schema_name"));
-CREATE TABLE "TAP_SCHEMA"."tables" ("schema_name" VARCHAR(1000), "table_name" VARCHAR(1000), "table_type" VARCHAR(1000), "description" VARCHAR(1000), "utype" VARCHAR(1000), PRIMARY KEY("table_name"));
-CREATE TABLE "TAP_SCHEMA"."columns" ("table_name" VARCHAR(1000), "column_name" VARCHAR(1000), "description" VARCHAR(1000), "unit" VARCHAR(1000), "ucd" VARCHAR(1000), "utype" VARCHAR(1000), "datatype" VARCHAR(1000), "size" INTEGER, "principal" INTEGER, "indexed" INTEGER, "std" INTEGER, PRIMARY KEY("table_name","column_name"));
-CREATE TABLE "TAP_SCHEMA"."keys" ("key_id" VARCHAR(1000), "from_table" VARCHAR(1000), "target_table" VARCHAR(1000), "description" VARCHAR(1000), "utype" VARCHAR(1000), PRIMARY KEY("key_id"));
-CREATE TABLE "TAP_SCHEMA"."key_columns" ("key_id" VARCHAR(1000), "from_column" VARCHAR(1000), "target_column" VARCHAR(1000), PRIMARY KEY("key_id"));
+CREATE TABLE "TAP_SCHEMA"."schemas" ("schema_name" VARCHAR(MAX), "description" VARCHAR(MAX), "utype" VARCHAR(MAX), PRIMARY KEY("schema_name"));
+CREATE TABLE "TAP_SCHEMA"."tables" ("schema_name" VARCHAR(MAX), "table_name" VARCHAR(MAX), "table_type" VARCHAR(MAX), "description" VARCHAR(MAX), "utype" VARCHAR(MAX), PRIMARY KEY("table_name"));
+CREATE TABLE "TAP_SCHEMA"."columns" ("table_name" VARCHAR(MAX), "column_name" VARCHAR(MAX), "description" VARCHAR(MAX), "unit" VARCHAR(MAX), "ucd" VARCHAR(MAX), "utype" VARCHAR(MAX), "datatype" VARCHAR(MAX), "size" INTEGER, "principal" INTEGER, "indexed" INTEGER, "std" INTEGER, PRIMARY KEY("table_name","column_name"));
+CREATE TABLE "TAP_SCHEMA"."keys" ("key_id" VARCHAR(MAX), "from_table" VARCHAR(MAX), "target_table" VARCHAR(MAX), "description" VARCHAR(MAX), "utype" VARCHAR(MAX), PRIMARY KEY("key_id"));
+CREATE TABLE "TAP_SCHEMA"."key_columns" ("key_id" VARCHAR(MAX), "from_column" VARCHAR(MAX), "target_column" VARCHAR(MAX), PRIMARY KEY("key_id"));
 
 INSERT INTO "TAP_SCHEMA"."schemas" VALUES ('TAP_SCHEMA', 'Set of tables listing and describing the schemas, tables and columns published in this TAP service.', NULL);
 
@@ -13,31 +13,31 @@ INSERT INTO "TAP_SCHEMA"."tables" VALUES ('TAP_SCHEMA', 'TAP_SCHEMA.columns', 't
 INSERT INTO "TAP_SCHEMA"."tables" VALUES ('TAP_SCHEMA', 'TAP_SCHEMA.keys', 'table', 'List all foreign keys but provides just the tables linked by the foreign key. To know which columns of these tables are linked, see in TAP_SCHEMA.key_columns using the key_id.', NULL);
 INSERT INTO "TAP_SCHEMA"."tables" VALUES ('TAP_SCHEMA', 'TAP_SCHEMA.key_columns', 'table', 'List all foreign keys but provides just the columns linked by the foreign key. To know the table of these columns, see in TAP_SCHEMA.keys using the key_id.', NULL);
 
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.schemas', 'schema_name', 'schema name, possibly qualified', '', '', '', 'VARCHAR(1000)', -1, 1, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.schemas', 'description', 'brief description of schema', '', '', '', 'VARCHAR(1000)', -1, 0, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.schemas', 'utype', 'UTYPE if schema corresponds to a data model', '', '', '', 'VARCHAR(1000)', -1, 0, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.tables', 'schema_name', 'the schema name from TAP_SCHEMA.schemas', '', '', '', 'VARCHAR(1000)', -1, 1, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.tables', 'table_name', 'table name as it should be used in queries', '', '', '', 'VARCHAR(1000)', -1, 1, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.tables', 'table_type', 'one of: table, view', '', '', '', 'VARCHAR(1000)', -1, 0, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.tables', 'description', 'brief description of table', '', '', '', 'VARCHAR(1000)', -1, 0, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.tables', 'utype', 'UTYPE if table corresponds to a data model', '', '', '', 'VARCHAR(1000)', -1, 0, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'table_name', 'table name from TAP_SCHEMA.tables', '', '', '', 'VARCHAR(1000)', -1, 1, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'column_name', 'column name', '', '', '', 'VARCHAR(1000)', -1, 1, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'description', 'brief description of column', '', '', '', 'VARCHAR(1000)', -1, 0, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'unit', 'unit in VO standard format', '', '', '', 'VARCHAR(1000)', -1, 0, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'ucd', 'UCD of column if any', '', '', '', 'VARCHAR(1000)', -1, 0, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'utype', 'UTYPE of column if any', '', '', '', 'VARCHAR(1000)', -1, 0, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'datatype', 'ADQL datatype as in section 2.5', '', '', '', 'VARCHAR(1000)', -1, 0, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.schemas', 'schema_name', 'schema name, possibly qualified', '', '', '', 'VARCHAR(MAX)', -1, 1, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.schemas', 'description', 'brief description of schema', '', '', '', 'VARCHAR(MAX)', -1, 0, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.schemas', 'utype', 'UTYPE if schema corresponds to a data model', '', '', '', 'VARCHAR(MAX)', -1, 0, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.tables', 'schema_name', 'the schema name from TAP_SCHEMA.schemas', '', '', '', 'VARCHAR(MAX)', -1, 1, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.tables', 'table_name', 'table name as it should be used in queries', '', '', '', 'VARCHAR(MAX)', -1, 1, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.tables', 'table_type', 'one of: table, view', '', '', '', 'VARCHAR(MAX)', -1, 0, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.tables', 'description', 'brief description of table', '', '', '', 'VARCHAR(MAX)', -1, 0, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.tables', 'utype', 'UTYPE if table corresponds to a data model', '', '', '', 'VARCHAR(MAX)', -1, 0, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'table_name', 'table name from TAP_SCHEMA.tables', '', '', '', 'VARCHAR(MAX)', -1, 1, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'column_name', 'column name', '', '', '', 'VARCHAR(MAX)', -1, 1, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'description', 'brief description of column', '', '', '', 'VARCHAR(MAX)', -1, 0, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'unit', 'unit in VO standard format', '', '', '', 'VARCHAR(MAX)', -1, 0, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'ucd', 'UCD of column if any', '', '', '', 'VARCHAR(MAX)', -1, 0, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'utype', 'UTYPE of column if any', '', '', '', 'VARCHAR(MAX)', -1, 0, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'datatype', 'ADQL datatype as in section 2.5', '', '', '', 'VARCHAR(MAX)', -1, 0, 0, 1);
 INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', '"size"', 'length of variable length datatypes', '', '', '', 'INTEGER', -1, 0, 0, 1);
 INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'principal', 'a principal column; 1 means true, 0 means false', '', '', '', 'INTEGER', -1, 0, 0, 1);
 INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'indexed', 'an indexed column; 1 means true, 0 means false', '', '', '', 'INTEGER', -1, 0, 0, 1);
 INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.columns', 'std', 'a standard column; 1 means true, 0 means false', '', '', '', 'INTEGER', -1, 0, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.keys', 'key_id', 'unique key identifier', '', '', '', 'VARCHAR(1000)', -1, 1, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.keys', 'from_table', 'fully qualified table name', '', '', '', 'VARCHAR(1000)', -1, 0, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.keys', 'target_table', 'fully qualified table name', '', '', '', 'VARCHAR(1000)', -1, 0, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.keys', 'description', 'description of this key', '', '', '', 'VARCHAR(1000)', -1, 0, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.keys', 'utype', 'utype of this key', '', '', '', 'VARCHAR(1000)', -1, 0, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.key_columns', 'key_id', 'unique key identifier', '', '', '', 'VARCHAR(1000)', -1, 1, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.key_columns', 'from_column', 'key column name in the from_table', '', '', '', 'VARCHAR(1000)', -1, 0, 0, 1);
-INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.key_columns', 'target_column', 'key column name in the target_table', '', '', '', 'VARCHAR(1000)', -1, 0, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.keys', 'key_id', 'unique key identifier', '', '', '', 'VARCHAR(MAX)', -1, 1, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.keys', 'from_table', 'fully qualified table name', '', '', '', 'VARCHAR(MAX)', -1, 0, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.keys', 'target_table', 'fully qualified table name', '', '', '', 'VARCHAR(MAX)', -1, 0, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.keys', 'description', 'description of this key', '', '', '', 'VARCHAR(MAX)', -1, 0, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.keys', 'utype', 'utype of this key', '', '', '', 'VARCHAR(MAX)', -1, 0, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.key_columns', 'key_id', 'unique key identifier', '', '', '', 'VARCHAR(MAX)', -1, 1, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.key_columns', 'from_column', 'key column name in the from_table', '', '', '', 'VARCHAR(MAX)', -1, 0, 0, 1);
+INSERT INTO "TAP_SCHEMA"."columns" VALUES ('TAP_SCHEMA.key_columns', 'target_column', 'key column name in the target_table', '', '', '', 'VARCHAR(MAX)', -1, 0, 0, 1);
 
