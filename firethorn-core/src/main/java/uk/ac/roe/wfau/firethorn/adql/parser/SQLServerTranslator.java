b62cea9ca1da7bae4bad56573afaff56395c10da
stvoutsin <steliosvoutsinas@yahoo.com>
2016-08-13 01:14:32 +0300

    Testing changes from Stsci

--- firethorn-core/src/main/java/uk/ac/roe/wfau/firethorn/adql/parser/SQLServerTranslator.java
+++ firethorn-core/src/main/java/uk/ac/roe/wfau/firethorn/adql/parser/SQLServerTranslator.java
@@ -29,14 +29,19 @@ import adql.db.exception.UnresolvedJoinException;
 import adql.query.ADQLList;
 import adql.query.ADQLObject;
 import adql.query.ADQLQuery;
+import adql.query.ClauseConstraints;
 import adql.query.ClauseSelect;
 import adql.query.IdentifierField;
 import adql.query.SelectAllColumns;
 import adql.query.SelectItem;
+import adql.query.constraint.Comparison;
 import adql.query.constraint.ConstraintsGroup;
 import adql.query.from.ADQLJoin;
 import adql.query.from.ADQLTable;
 import adql.query.operand.ADQLColumn;
+import adql.query.operand.ADQLOperand;
+import adql.query.operand.NegativeOperand;
+import adql.query.operand.NumericConstant;
 import adql.query.operand.function.ADQLFunction;
 import adql.query.operand.function.CastFunction;
 import adql.query.operand.function.MathFunction;
@@ -92,7 +97,12 @@ import adql.db.DBType.DBDatatype;
 public class SQLServerTranslator extends PostgreSQLTranslator implements ADQLTranslator {
 
 	private final String schemaName = "dbo";
-
+	//The fnSpatial functions used for geometry at MAST return a table 
+	//which is a join of the ivoa.obscore table and the geometric search results.
+	//It has all the columns of the ivoa.obscore table.
+	//In order to translate complex queries, we need to give this temporary table a unique name.
+	private static String functionTableName = "obscoreTempResults";
+	private static String baseTableName = "ivoa.obscore";
 	/**
 	 *
 	 *
@@ -120,35 +130,6 @@ public class SQLServerTranslator extends PostgreSQLTranslator implements ADQLTra
 
 	}
 
-	/**
-	 * Replaces the PostgreSQLTranslator method to not put LIMIT at the end.
-	 *
-	 */
-	@Override
-	public String translate(final ADQLQuery query) throws TranslationException {
-		log.debug("translate(ADQLQuery)");
-		final StringBuilder builder = new StringBuilder();
-		builder.append(translate(query.getSelect()));
-
-		builder.append("\nFROM ").append(translate(query.getFrom()));
-
-		if (!query.getWhere().isEmpty()) {
-			builder.append('\n').append(translate(query.getWhere()));
-		}
-
-		if (!query.getGroupBy().isEmpty()) {
-			builder.append('\n').append(translate(query.getGroupBy()));
-		}
-
-		if (!query.getHaving().isEmpty()) {
-			builder.append('\n').append(translate(query.getHaving()));
-		}
-
-		if (!query.getOrderBy().isEmpty()) {
-			builder.append('\n').append(translate(query.getOrderBy()));
-		}
-		return builder.toString();
-	}
 
 	/**
 	 * Replaces the PostgreSQLTranslator method to put TOP at the beginning.
@@ -500,66 +481,174 @@ public class SQLServerTranslator extends PostgreSQLTranslator implements ADQLTra
 	}
 	
 	
-	@Override
-	public String translate(final ExtractCoord extractCoord) throws TranslationException {
-		return getDefaultADQLFunction(extractCoord);
+	//If the query has a CONTAINS geometric function in it, an extensive rewrite is needed
+	//for the Obscore specific spatial functions.
+	//Due to the expense of searching for said geometric functions, this check is only done once,
+	//and then specific Geometry-related translation functions defined here are used.
+	//
+	//Do not call the WithGeometry functions on queries that *may* not have geometry; 
+	//translation errors will be incorrectly generated.
+	private boolean queryHasGeometry(ADQLQuery query) throws TranslationException {
+		if( query != null && 
+			query.getWhere() != null && 
+			findGeometryFunction(query.getWhere()) != null)
+			return true;
+		return false;
+	}
+	
+	private ADQLFunction findGeometryFunction(ADQLList<? extends ADQLObject> list) throws TranslationException {
+		Comparison comp;
+		GeometryFunction fn = null;
+		ADQLOperand comparator = null;
+		for( int i = 0; i < list.size(); ++i) {
+			if(list.get(i) instanceof Comparison) {
+				comp = (adql.query.constraint.Comparison)list.get(i);
+				if( comp.getLeftOperand() instanceof GeometryFunction) {
+					fn = (GeometryFunction)comp.getLeftOperand();
+					comparator = (ADQLOperand)comp.getRightOperand();
+				}
+				if( comp.getRightOperand() instanceof GeometryFunction) {
+					fn = (GeometryFunction)comp.getRightOperand();
+					comparator = (ADQLOperand)comp.getLeftOperand();
+				}
+			}
+			//quick test for validity against what our translator handles.
+			if( fn != null) {
+				if( comparator instanceof NegativeOperand || ((NumericConstant)comparator).getNumericValue() > 1)
+					throw new TranslationException("ADQL geometric queries can only be compared to 1 (true) or 0 (false). False is not currently supported.");
+				if (((NumericConstant)comparator).getNumericValue() == 0)
+					throw new TranslationException("ADQL geometric function support is currently limited to tests against 1 (true)");
+				
+				return (ADQLFunction)fn;
+			}
+		}	
+		return null;
+	}
+	
+	public String translateSelectWithGeometry(ClauseSelect clause) throws TranslationException{
+		String sql = null;
+		
+		//tdower todo: this, cleanly
+		sql = translate(clause);
+		return sql.replace(baseTableName, functionTableName);
+	}
+	
+	private String translateFunctionFromWhereWithGeometry(ADQLList<? extends ADQLObject> list) throws TranslationException {
+		return translate(findGeometryFunction(list)) + " as " + functionTableName;
+	}
+
+	//Finds geometry comparison in where. If there is one, removes it. 
+	//If there's anything left, translates the rest of the where clause by usual rules.
+	private String translateRestOfWhereWithGeometry(ClauseConstraints where) throws TranslationException {
+		if(findGeometryFunction(where) != null) {
+			try {
+				Comparison comp;
+				for( int i = 0; i < where.size(); ++i) {
+					comp = (adql.query.constraint.Comparison)where.get(i);
+					if( comp.getLeftOperand() instanceof GeometryFunction || comp.getRightOperand() instanceof GeometryFunction) {
+						if( where.size() > 1 ) {
+							where.remove(i);
+							break;
+						}
+						else
+							return "";
+					}
+				}
+			} catch (Exception e) {
+				throw new TranslationException("Unexpected issue translating non-geometry section of WHERE constraints: " + e.getMessage());
+			}
+			
+			//tdower todo: this, cleanly.
+			String sql = translate(where);
+			return sql.replace(baseTableName, functionTableName);
+		}
+		else
+			return translate(where);
 	}
 
 	@Override
-	public String translate(final ExtractCoordSys extractCoordSys) throws TranslationException {
-		return getDefaultADQLFunction(extractCoordSys);
+	public String translate(ADQLQuery query) throws TranslationException{
+		//Normally the SQLServerTranslator functionality will suffice.
+		if(!queryHasGeometry(query)) {
+			return super.translate(query);
+		}
+		//but if the query contains geometry, 
+		//it needs a rewrite with respect to the spatial function temporary table.
+		else {
+			StringBuffer sql = new StringBuffer(translateSelectWithGeometry(query.getSelect()));
+
+			sql.append("\nFROM ");
+			sql.append(translateFunctionFromWhereWithGeometry(query.getWhere()));
+			
+			sql.append('\n').append(translateRestOfWhereWithGeometry(query.getWhere()));
+
+			if (!query.getGroupBy().isEmpty())
+				sql.append('\n').append(translate(query.getGroupBy()));
+
+			if (!query.getHaving().isEmpty())
+				sql.append('\n').append(translate(query.getHaving()));
+
+			if (!query.getOrderBy().isEmpty())
+				sql.append('\n').append(translate(query.getOrderBy()));
+
+			return sql.toString();
+		}
 	}
 
 	@Override
-	public String translate(final AreaFunction areaFunction) throws TranslationException {
-		return getDefaultADQLFunction(areaFunction);
-	}
-
-	@Override
-	public String translate(final CentroidFunction centroidFunction) throws TranslationException {
-		return getDefaultADQLFunction(centroidFunction);
-	}
-
-	@Override
-	public String translate(final DistanceFunction fct) throws TranslationException {
-		return getDefaultADQLFunction(fct);
-	}
-
-	@Override
+	//we will be ignoring the Contains() in favor of using the fnSpatial syntax 
+	//for the underlying spatial function.
 	public String translate(final ContainsFunction fct) throws TranslationException {
-		return getDefaultADQLFunction(fct);
+		return(translate(fct.getLeftParam()));
 	}
 
 	@Override
 	public String translate(final IntersectsFunction fct) throws TranslationException {
-		return getDefaultADQLFunction(fct);
+		throw new TranslationException("INTERSECTS queries not currently supported; only CONTAINS");
 	}
 
 	@Override
 	public String translate(final PointFunction point) throws TranslationException {
-		return getSQLServerADQLFunction(point, "dbo.fnSpatial_SearchPoint");
-
+		return ("dbo.fnObsCore_SearchSTCSFootprint('point " +
+				((NumericConstant)(point.getCoord1())).getValue() + " " +
+				((NumericConstant)(point.getCoord2())).getValue() + "')"); 
 	}
 
 	@Override
 	public String translate(final CircleFunction circle) throws TranslationException {
-		return getSQLServerADQLFunction(circle, "dbo.fnSpatial_SearchCircle");
+		return ("dbo.fnObsCore_SearchSTCSFootprint('circle " +
+				circle.getCoord1().toADQL() + " " +
+				circle.getCoord2().toADQL() + " " +
+				circle.getRadius().toADQL() + "')");
 	}
 
 	@Override
 	public String translate(final BoxFunction box) throws TranslationException {
-		return getSQLServerADQLFunction(box, "dbo.fSearchBox");
-
+		return ("dbo.fnObsCore_SearchSTCSFootprint('box " +
+				box.getCoord1().toADQL() + " " +
+				box.getCoord2().toADQL() + " " +
+				box.getWidth().toADQL() + " " +
+				box.getHeight().toADQL() + "')");
 	}
 
 	@Override
 	public String translate(final PolygonFunction polygon) throws TranslationException {
-		return getDefaultADQLFunction(polygon);
+		StringBuilder fnCall = new StringBuilder("dbo.fnObsCore_SearchSTCSFootprint('polygon ");
+		ADQLOperand[] operands = polygon.getParameters();
+		for( int i = 0; i < operands.length; ++i) {
+			if( operands[i] instanceof NumericConstant || operands[i] instanceof NegativeOperand)
+				fnCall.append(operands[i].toADQL());
+			if( i < operands.length - 1 )
+				fnCall.append(" ");
+		}
+		
+		fnCall.append("') ");
+		return fnCall.toString();
 	}
 
 	@Override
 	public String translate(final RegionFunction region) throws TranslationException {
-		return getDefaultADQLFunction(region);
+		throw new TranslationException("Region queries are not currently supported, only POINT, CIRCLE, BOX, and POLYGON.");
 	}
 
 	@Override
