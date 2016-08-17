d565bd1b90604d5caec732f40bbad99866233624
Dave Morris <zrq-github@metagrid.co.uk>
2016-08-17 14:08:44 +0100

    Reomved AdqlQueryDatatablesController and AdqlQueryVOTableController

--- firethorn-webapp/src/main/java/uk/ac/roe/wfau/firethorn/webapp/tap/TapError.java
+++ firethorn-webapp/src/main/java/uk/ac/roe/wfau/firethorn/webapp/tap/TapError.java
@@ -1,25 +1,7 @@
 package uk.ac.roe.wfau.firethorn.webapp.tap;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-
-import uk.ac.roe.wfau.firethorn.adql.query.green.GreenQuery;
-import uk.ac.roe.wfau.firethorn.meta.adql.AdqlColumn;
-import uk.ac.roe.wfau.firethorn.meta.adql.AdqlTable;
-import uk.ac.roe.wfau.firethorn.meta.base.BaseColumn;
-import uk.ac.roe.wfau.firethorn.meta.jdbc.JdbcProductType;
-import uk.ac.roe.wfau.firethorn.meta.jdbc.JdbcTable;
-import uk.ac.roe.wfau.firethorn.webapp.votable.AdqlQueryVOTableController.FieldHandler;
-import uk.ac.roe.wfau.firethorn.webapp.votable.AdqlQueryVOTableController.ObjectFieldHandler;
-import uk.ac.roe.wfau.firethorn.webapp.votable.AdqlQueryVOTableController.StringFieldHandler;
  
 public class TapError {
 	 
