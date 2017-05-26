c2ca6947782f5a4cd5180e0cbe5dcf40701b3dab
Dave Morris <zrq-github@metagrid.co.uk>
2017-05-26 12:29:52 +0100

    Combining tunnel and proxy

--- docker/sql-proxy/bin/sql-proxy.sh
+++ docker/sql-proxy/bin/sql-proxy.sh
@@ -21,11 +21,115 @@
 #
 #
 
-targetip=$(host -t A ${target:?} | cut -d ' ' -f 4)
-echo "Target name [${target:?}]"
-echo "Target IP   [${targetip:?}]"
+#
+# Fix for different param names.
+if [ -n "${target}" ]
+then
+    targethost=${target}
+fi
+
+echo "Target host [${targethost}]"
+echo "Tunnel host [${tunnelhost}]"
+echo "Tunnel user [${tunneluser}]"
 
 #
-# Proxy a SQLServer connection.
-socat "TCP-LISTEN:1433,fork,reuseaddr" "TCP:${targetip:?}:1433"
+# Get our external IP address.
+extern()
+    {
+    curl -4 --silent 'http://icanhazip.com/'
+    }
+
+#
+# Check the route to a specific host.
+route()
+    {
+    local target=${1:?}
+    ip route get "${target:?}" |
+    sed -n '
+        /^'${target}'/ {
+            s/^'${target}' via \([0-9.]*\)\b.*$/\1/p
+            }
+        '
+    }
+
+#
+# Proxy a connection.
+proxy()
+    {
+    local targethost=${1:?}
+
+    echo "Proxy connection"
+    echo "Target host [${targethost}]"
+
+    targetip=$(host -t A ${targethost:?} | cut -d ' ' -f 4)
+
+    echo "Target IP   [${targetip:?}]"
+
+    socat "TCP-LISTEN:1433,fork,reuseaddr" "TCP:${targetip:?}:1433"
+    }
+
+#
+# Tunnel a connection over SSH.
+tunnel()
+    {
+    local targethost=${1:?}
+    local tunnelhost=${2:?}
+    local tunneluser=${3:?}
+
+    echo "SSH connection"
+    echo "Target host [${targethost}]"
+    echo "Tunnel host [${tunnelhost}]"
+    echo "Tunnel user [${tunneluser}]"
+
+    export SSH_AUTH_SOCK=/tmp/ssh_auth_sock
+    ssh-keyscan "${tunnelhost:?}" 2> /dev/null >> /etc/ssh/ssh_known_hosts
+
+    ssh -v -C -L "*:1433:${targethost:?}:1433" "${tunneluser:?}@${tunnelhost:?}"
+    }
+
+#
+# Get our external IP address.
+external=$(extern)
+echo "External IP [${external}]"
+
+#
+# If our address is in ROE.
+if [[ ${external:?} == 129.215.* ]]
+then
+    echo "INTERNAL network"
+    #
+    # Check the route to ramses1.
+    via=$(route '192.168.137.20')
+    echo "VIA [${via}]"
+    #
+    # If the route is direct (has no via).
+    if [ -z ${via} ]
+    then
+        echo "DIRECT route"
+        #
+        # Proxy the SQLServer connection.
+        proxy(
+            ${targethost}
+            )
+    else
+        echo "INDIRECT route"
+        #
+        # Tunnel the SQLServer connection.
+        tunnel(
+            ${targethost}
+            ${tunnelhost}
+            ${tunneluser}
+            )
+    fi
+else
+    echo "EXTERNAL network"
+    #
+    # Tunnel the SQLServer connection.
+    tunnel(
+        ${targethost}
+        ${tunnelhost}
+        ${tunneluser}
+        )
+fi
+
 
