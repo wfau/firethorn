df17ae0a1e8f75990e885b918a4e958e824f4eac
Dave Morris <zrq-github@metagrid.co.uk>
2017-08-28 14:10:03 +0100

    Removed old GUI code

--- firethorn-gui/OSA-gui/freeform_sql/__init__.py
+++
@@ -1,8 +0,0 @@
-import html_functions
-import xml_parser
-from freeform_sql.freeform_config import * #@UnusedWildImport
-from file_handler import File_handler
-from matplotlib_plots import Matplotlib_plots
-from misc import *
-from firethorn_config import * 
-from firethorn_helpers import *
