fc282485821ae32bc730b00de70306725fa22552
stvoutsin <steliosvoutsinas@yahoo.com>
2020-03-03 23:13:25 +0200

    Changed Byte to Integer for TINYINTs in AdqlColumn

--- firethorn-core/src/main/java/uk/ac/roe/wfau/firethorn/meta/adql/AdqlColumn.java
+++ firethorn-core/src/main/java/uk/ac/roe/wfau/firethorn/meta/adql/AdqlColumn.java
@@ -259,7 +259,7 @@ extends BaseColumn<AdqlColumn>
                 case TIMESTAMP :
                     return AdqlColumn.AdqlType.DATETIME;
                 case TINYINT :
-                    return AdqlColumn.AdqlType.BYTE;
+                    return AdqlColumn.AdqlType.INTEGER;
 
 
                 case BLOB :
