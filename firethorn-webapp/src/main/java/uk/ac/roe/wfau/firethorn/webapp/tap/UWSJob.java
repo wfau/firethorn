d3c095b615d92564c1b56469335ac10d164283bd
stvoutsin <steliosvoutsinas@yahoo.com>
2020-07-28 15:01:06 +0300

    Add CDATA Wrapper around query in UWS Job output

--- firethorn-webapp/src/main/java/uk/ac/roe/wfau/firethorn/webapp/tap/UWSJob.java
+++ firethorn-webapp/src/main/java/uk/ac/roe/wfau/firethorn/webapp/tap/UWSJob.java
@@ -553,8 +553,8 @@ public class UWSJob {
 			        } else {
 		            	writer.append("<uws:parameter id='lang'>None</uws:parameter>");
 			        }
-			        if (this.getQuery()!=null){
-		            	writer.append("<uws:parameter id='query'>" + this.getQuery().input() + "</uws:parameter>");
+			        if (this.getQuery()!=null){         
+		            	writer.append("<uws:parameter id='query'> <![CDATA[" + this.getQuery().input() + "]]></uws:parameter>");
 			        }
 			        if (this.getFormat()!=null){
 		            	writer.append("<uws:parameter id='format'>" + this.getFormat() + "</uws:parameter>");
